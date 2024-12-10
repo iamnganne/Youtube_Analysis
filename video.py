1ac5d446d5bb617f67d3ebd8d1990548303fc756
Nam1392003 <117732946+Nam1392003@users.noreply.github.com>
2024-12-10 17:08:02 +0700

    sua code api

--- video.py
+++ video.py
@@ -1,6 +1,6 @@
-import os 
-import googleapiclient.discovery 
-import json 
+import os
+import googleapiclient.discovery
+import json
 
 def main():
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
@@ -9,16 +9,29 @@ def main():
     DEVELOPER_KEY = "AIzaSyAHENUMIslTQ3FKidvMg2wN7TGkaCoeMNU"
 
     youtube = googleapiclient.discovery.build(
-        api_service_name,api_version,developerKey = DEVELOPER_KEY
+        api_service_name, api_version, developerKey=DEVELOPER_KEY
     )
+
+    # Danh sách video IDs
+    video_ids = [
+        "lpHZsaH_nfE", "XwY9ysGXkI4", "KJApEC8hGTk",
+        "0gwVK2nUjYY", "AZ508saosRY", "L4HWqGQxIj4",
+        "934Upp7Efw0", "QbLxZQpMwug", "2m2P4QCCsEo"
+    ]
+
+    # Tạo một chuỗi video ID phân tách bằng dấu phẩy
+    video_id_string = ",".join(video_ids)
+
+    # Gửi yêu cầu API
     request = youtube.videos().list(
-       part="snippet,contentDetails,statistics",
-        id="KMdlKttaWJY,LrNiiFPKA90,rcgrVgVlV5c,S07VinbxN3g,IxchuaO_frY",
-        maxResults=50
+        part="snippet,contentDetails,statistics",
+        id=video_id_string
     )
-    
     response = request.execute()
+
+    # Lưu kết quả vào file JSON
     with open("video.json", "w", encoding="utf-8") as f:
         json.dump(response, f, indent=4, ensure_ascii=False)
+
 if __name__ == "__main__":
     main()
